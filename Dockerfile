96c721ef4160e88f2a220d0c332f0afbebbad13f
Tomokazu Kiyohara <tomokazu.kiyohara@gmail.com>
2014-10-28 12:56:23 +0900

    add shell scripts

--- Dockerfile
+++ Dockerfile
@@ -3,13 +3,20 @@ FROM kiyohara/docker-ruby
 MAINTAINER Tomokazu Kiyohara <tomokazu.kiyohara@gmail.com>
 
 RUN apt-get install -y ipmitool
+RUN apt-get install -y freeipmi
 
 RUN gem install --no-ri --no-rdoc rubyipmi
 RUN gem install --no-ri --no-rdoc mqtt
 
 RUN git clone https://github.com/kiyohara/ipmi_mqtt_sample.git /tmp/ipmi_mqtt_sample
+
+# ruby
 RUN cd /tmp/ipmi_mqtt_sample/ruby; rake build
 RUN gem install --no-ri --no-rdoc /tmp/ipmi_mqtt_sample/ruby/pkg/ipmi_mqtt_sample-*.gem
+
+# shell
+RUN cp /tmp/ipmi_mqtt_sample/shell/*.sh /usr/local/bin/
+
 RUN rm -rf /tmp/ipmi_mqtt_sample
 
 CMD [ "/usr/local/bin/ipmi_mqtt_sample" ]
---
+++ shell/ipmi-poll.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+EXEC_CMD=ipmi-sensors
+OUTPUT_DIR=/tmp
+
+if ! `which $EXEC_CMD 2>&1 > /dev/null`; then
+  echo ERROR: freeipmi required
+  exit 1
+fi
+
+[ -z "$IPMI_TARGET" ] && IPMI_TARGET=localhost
+EXEC_ARGS="-h $IPMI_TARGET"
+[ -n "$IPMI_USER" ] && EXEC_ARGS="$EXEC_ARGS -u $IPMI_USER"
+[ -n "$IPMI_PASS" ] && EXEC_ARGS="$EXEC_ARGS -p $IPMI_PASS"
+
+# EXEC_ARGS="$EXEC_ARGS --no-header-output --comma-separated-output"
+EXEC_ARGS="$EXEC_ARGS --comma-separated-output"
+
+while true; do
+  STR_DATE=`date +%Y%m%d-%H%M%S-%N`
+  OUTPUT_FILE=$OUTPUT_DIR/$EXEC_CMD-$STR_DATE
+
+  $EXEC_CMD $EXEC_ARGS > $OUTPUT_FILE
+
+  sleep 1
+done
